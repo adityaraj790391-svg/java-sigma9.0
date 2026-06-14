ad74f99203e0d862a58545bda798fef90eac0a0a
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-14 22:31:10 +0530

    done

--- loops.java
+++ loops.java
@@ -133,12 +133,12 @@ public class loops {
 
             // Continue Statement
 
-    // for (int i =1; i<=10; i++) {
-    //     if(i==5) {
-    //         continue;
-    //     }
-    //     System.out.println(i);
-    // }
+    for (int i =1; i<=10; i++) {
+        if(i==5) {
+            continue;
+        }
+        System.out.println(i);
+    }
 
 
           // Display all nos. except multiples of 10
