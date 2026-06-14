1ed2f0bf3d935d39ce96ef9107e48a2d66afa687
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-14 22:31:30 +0530

    done

--- loops.java
+++ loops.java
@@ -109,12 +109,12 @@ public class loops {
     
            // Break statement
 
-    // for (int i =1; i<=10; i++) {
-    //     if(i==5) {
-    //         break;
-    //     }
-    //     System.out.println(i);
-    // }
+    for (int i =1; i<=10; i++) {
+        if(i==5) {
+            break;
+        }
+        System.out.println(i);
+    }
 
 
 
