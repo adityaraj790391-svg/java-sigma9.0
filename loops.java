0c801c879962db6620cfe33eefcd93e8b60180b0
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-14 22:32:52 +0530

    done

--- loops.java
+++ loops.java
@@ -87,13 +87,14 @@ public class loops {
                // Reverse the given no.
 
       //   int n = 12345;
-        int reverse = 0;
-        while (n>0) {
+
+      int reverse = 0;
+      while (n>0) {
             int lastDigit = n%10;
             reverse = (reverse*10) + lastDigit;
             n = n/10; 
-        }
-        System.out.println(reverse);
+      }
+      System.out.println(reverse);
 
 
               // DO WHILE LOOP
