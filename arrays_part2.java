cb9a69ed26172a6785485586add617e56222cd3b
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-29 13:42:03 +0530

    done

--- arrays_part2.java
+++ arrays_part2.java
@@ -36,7 +36,7 @@ public class arrays_part2 {
     // }
 
 
-           //// Max Sub Array - Two -   Prefix Array - using formula for max array sum - prefix[end] - prefix[start-1]
+           //// Max Sub Array - Two -   Prefix Array - using formula for max array sum - prefix[end] - prefix[start-1] - using formula for prefix sum - prefix[i-1] - arr[i]
 
 
 
